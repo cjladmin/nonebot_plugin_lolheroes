52b9bdfae1c9a8c133203163bd332529f935e3d7
Torres-圣君 <53791401+cjladmin@users.noreply.github.com>
2021-11-18 11:45:11 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,26 @@
+<p align="center">
+  <a href="https://v2.nonebot.dev/"><img src="https://avatars.githubusercontent.com/u/53791401?v=4" width="200" height="200" alt="nonebot"></a>
+</p>
+
+<div align="center">
+
+# LOLHeroes
+
+_✨ 基于NoneBot2实现的 获取LOL英雄背景故事 插件 ✨_
+
+</div>
+
+## 配置说明：
+
+> 1.把下载后的文件夹放在`src/plugins/`路径下
+
+> 2.在`bot.py`文件内添加：`nonebot.load_plugin("lolheroes")`
+
+## 使用说明：
+
+> 直接在群内/私聊发送：
+>> lol亚索/疾风剑豪背景故事
+
+ - 效果如图所示
+ 
+![效果如图所示](data/yasuo.jpg)
---
+++ lolheroes/__init__.py
@@ -0,0 +1,45 @@
+# -*- codeing -*-
+# @Time : 2021/11/15 0:14
+# @Author : Torres-圣君
+# @File : __init__.py
+# @Sofaware : PyCharm
+from nonebot.plugin import on_regex
+from nonebot.adapters.cqhttp import Bot, Event
+from nonebot.adapters.cqhttp.message import Message
+import requests
+import json
+
+
+# 发送'lolxxx背景故事'，例：lol亚索背景故事
+test = on_regex(r".*(lol).*(背景故事).*", priority=49)
+
+@test.handle()
+async def _(bot: Bot, event: Event):
+    msg = str(event.get_message()).strip("lol背景故事 ")
+    try:
+        with open(r"data\heroesID.json", "r", encoding='utf-8') as r:
+            json_data = json.load(r)
+            name_url = json_data[msg]
+            hero_url = json_data[msg].split("/")
+            # 获取英雄对应的英文名
+            hero_en = hero_url[-2]
+        title_name = hero_en.title()
+        photo = f'[CQ:image,file=https://game.gtimg.cn/images/lol/universe/v1/assets/images/champion/splash/{title_name}_0.jpg]'
+        await test.finish(message=Message((photo + run(hero_en) +
+                                           "\n——————————————————————\n" +
+                                           msg + "更多内容请点击：\n" +
+                                           name_url)))
+    except Exception:
+        pass
+        # await test.finish(message=Message("您输入的英雄名字有误，或不存在."), at_sender=True)
+
+
+# 获取英雄背景故事
+def run(name_en):
+    url = f"https://yz.lol.qq.com/v1/zh_cn/champions/{name_en}/index.json"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 Edg/95.0.1020.53"
+    }
+    res = requests.get(url, headers)
+    name_data = res.json()
+    return name_data["champion"]["biography"]["short"].strip("</p>")
